cb65a181bdd8a108269ee17fc88c251390610a94
hongkewei <hongkewei@bytedance.com>
2020-12-16 21:51:11 +0800

    feat: add comment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import { createMemoryHistory, createRouter } from "vue-router";
+import App from "./App.vue";
+import "./index.css";
+// TODO 初始化
+const routes = [];
+const history = createMemoryHistory();
+const router = createRouter({ history, routes });
+createApp(App).mount("#app");
