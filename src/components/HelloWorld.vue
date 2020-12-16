4d4565d86ca65a7dab51bd3e82e7249e774cba00
hongkewei <hongkewei@bytedance.com>
2020-12-16 21:49:32 +0800

    Initial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="plus">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div>{{ cv }}</div>
+</template>
+
+<script>
+import { toRefs, ref, computed, watchEffect, reactive } from "vue";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    
+  }
+};
+</script>
